0134decb21feb77fd54d9467e1d968e1251f12b1
Bashamega <adambashaahmednaji@gmail.com>
2025-05-19 11:24:58 +0300

    generate

--- generated/DistanceUnits/index.d.ts
+++ generated/DistanceUnits/index.d.ts
@@ -2,5 +2,6 @@ import type { LocalFontRelativeLengths } from './LocalFontRelativeLengths';
 import type { RootFontRelativeLengths } from './RootFontRelativeLengths';
 import type { ViewportUnits } from './ViewportUnits';
 import type { ContainerUnits } from './ContainerUnits';
+import type { Percentages } from './Percentages';
 
-export type DistanceUnits = LocalFontRelativeLengths | RootFontRelativeLengths | ViewportUnits | ContainerUnits;
+export type DistanceUnits = LocalFontRelativeLengths | RootFontRelativeLengths | ViewportUnits | ContainerUnits | Percentages;
